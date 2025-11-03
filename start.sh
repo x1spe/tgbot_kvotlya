1c9829befd425ebd80d02c504436b1a714dc2592
x1spe <vladimirevstafjev2010@gmail.com>
2025-11-04 01:17:46 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,12 @@
 pip install --upgrade pip setuptools wheel
 
 # Устанавливаем зависимости
-pip install -r requirements.txt
+pip install python-telegram-bot==13.15
+pip install Pillow==12.0.0
+pip install requests==2.31.0
 
 # Запуск бота
 python3 main.py
+
 
 chmod +x start.sh
